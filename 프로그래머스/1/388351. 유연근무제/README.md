4241a411ec157f83cbfc5db6c490489e057d9d4b
정인호 <108401188+MASKUN2@users.noreply.github.com>
2025-06-02 20:25:46 +0900

    [level 1] Title: 유연근무제, Time: 0.41 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 66.4 MB, 시간: 0.53 ms
+메모리: 67.6 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 02일 20:18:53
+2025년 06월 02일 20:25:44
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.kt
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.kt
@@ -4,15 +4,12 @@ class Solution {
         for(i in schedules.indices){
             var schedule = schedules[i].plus(10)
             if(schedule.mod(100) >= 60){
-                schedule = schedule.div(100)*100 + 100 + schedule.mod(100) - 60
+                schedule = schedule.div(100).plus(1).times(100).plus(schedule.mod(100).minus(60))
             }
             for(j in 0..6){
-                val day = (startday - 1 + j) % 7
-                if(day == 5 || day == 6) continue
-                val timelog = timelogs[i][j]
-                if(timelog <= schedule){
-                    continue
-                }else{
+                val day = (startday + j) % 7
+                if(day == 6 || day == 0) continue
+                if(timelogs[i][j] > schedule){
                     answer--
                     break
                 }
