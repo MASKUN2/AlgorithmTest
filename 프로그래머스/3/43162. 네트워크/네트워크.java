8073dbd597a61b81ff48fe85b235eced98bbf1e2
정인호 <108401188+MASKUN2@users.noreply.github.com>
2025-12-03 02:21:45 +0900

    [level 3] Title: 네트워크, Time: 2.24 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,32 +3,60 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        Map<Integer,Set<Integer>> map = new HashMap<>();
+        var nw = new Network(n);
         
-        for(int i = 0; i < computers.length; i++){
-            map.put(i, new HashSet<>(Set.of(i)));
-        }
-        
-        for(int i = 0; i < computers.length; i++){
-            for(int j = 0; j < computers[i].length; j++){
-                if(computers[i][j] == 0 || j > i){
-                    continue;
-                }
-                
-                var iSet = map.get(i);
-                var jSet = map.get(j);
-                
-                iSet.addAll(jSet);
-                
-                for(Integer target : jSet){
-                    map.put(target, iSet);
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                if(computers[r][c] == 1){
+                    nw.wind(r,c);
                 }
             }
         }
         
-        var result = map.values().stream().collect(Collectors.toSet());
+        return nw.getCount();
+    }
+    
+    static class Network{
+        final HashSet[] connections;
         
-        return result.size();
+        Network(int nodeSize){
+            this.connections = new HashSet[nodeSize];
+            IntStream.range(0, nodeSize)
+                .forEach(i -> this.connections[i] = new HashSet<Integer>());
+        }
+        
+        void wind(int node1, int node2){
+            this.connections[node1].add(node2);
+            this.connections[node2].add(node1);
+        }
+        
+        int getCount(){
+            final int nodeSize = connections.length;
+            var visited = new boolean[nodeSize];
+         
+            int count = 0;
+            for(int i = 0; i < nodeSize; i++){
+                if(visited[i]){
+                    continue;
+                }
+                visitConnected(i, visited);
+                count++;
+            }
+            
+            return count;
+        }
+        
+        void visitConnected(int node, boolean[] visitMap){
+            if(visitMap[node]){
+                return;
+            }
+            
+            visitMap[node] = true;
+            
+            for(Integer connectedNode : (Set<Integer>) connections[node]){
+                visitConnected(connectedNode, visitMap);
+            }
+        }
         
     }
 }
